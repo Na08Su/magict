88e35c0666f0641202af6a549d7520850354328d
Natsuki <snsn19910803@gmail.com>
2016-12-13 19:51:05 +0900

    migrationファイルエラー修正

--- db/migrate/20160727045618_change_tag_in_task.rb
+++ db/migrate/20160727045618_change_tag_in_task.rb
@@ -1,5 +1,5 @@
 class ChangeTagInTask < ActiveRecord::Migration
   def change
-    change_column :tasks, :tag, :integer
+    change_column :tasks, :tag, 'integer USING CAST(tag AS integer)' #:integer
   end
 end
