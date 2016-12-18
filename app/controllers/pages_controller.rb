e514e7ecc4fbee7e1b665dc531273bf6c4a26348
Natsuki <snsn19910803@gmail.com>
2016-12-18 19:05:45 +0900

    landingページ beta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def about
   end
+
+  def landing
+    render :layout => false
+    # @users = User.all
+
+    # @user = User.new
+    # puts @user.nil?
+  end
 end
