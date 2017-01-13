342152c0a1b5149eb0a7162ae506a2b445b871b0
Natsuki <snsn19910803@gmail.com>
2017-01-13 12:29:14 +0900

    viewの調整, articleにpickup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,9 @@ class ArticlesController < ApplicationController
 	def index
 		set_search
 		@search_articles = @q.result.order(created_at: :desc)
-		@move_article = Article.order(created_at: :asc).limit(4) # jqueryで左右に流れる
-		@right_article = Article.find(2)
+		@move_article    = Article.order(created_at: :asc).limit(4) # jqueryで左右に流れる
+		@right_article   = Article.find(2)
+		@pick_up         = Article.order("RANDOM()").limit(3)
 
 		render :layout => false
 	end
