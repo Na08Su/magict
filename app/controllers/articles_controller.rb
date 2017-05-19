0d4f171b6ca22b30cf8148feeaeedf16451cd5a5
natsuki.sugawara <natsuki.sugawara@jmpc-059.local>
2017-05-19 15:35:11 +0900

    nilガード

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 		set_search
 		@search_articles = @q.result.order(created_at: :desc)
 		@move_article    = Article.order(created_at: :asc).limit(4) # jqueryで左右に流れる
-		@right_article   = Article.find(2)
+		@right_article   = Article.second if Article.second
 		@pick_up         = Article.order("RANDOM()").limit(3)
 
 		render :layout => false
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 	def show
 		set_search
 		@article = Article.find(params[:id])
-		@articles = @q.result.order(created_at: :desc).limit(6) #　ここの表示ロジックは後で考える  
+		@articles = @q.result.order(created_at: :desc).limit(6) #　ここの表示ロジックは後で考える
 		render :layout => false
 	end
 
